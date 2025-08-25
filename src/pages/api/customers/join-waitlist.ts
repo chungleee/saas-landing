17cbe4cb01d00bca6333ba0393d2a852527aa81d
Leon <leon.karho.chung@gmail.com>
2025-08-25 15:56:42 -0400

    Waitlist Target
    - dynamically make appropriate request based on waitlist target

---
+++ src/pages/api/customers/join-waitlist.ts
@@ -0,0 +1,21 @@
+import { Resend } from 'resend';
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+const resend = new Resend(import.meta.env.RESEND_CUSTOMER_API_KEY);
+
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json();
+
+  const response = await resend.contacts.create({
+    email: body.email,
+    audienceId: import.meta.env.RESEND_LOOPLY_CUSTOMER_AUDIENCE,
+  });
+
+  if (response.error) {
+    return new Response(JSON.stringify({ type: 'error', error: response.error }));
+  }
+
+  return new Response(JSON.stringify({ type: 'success', data: response.data }));
+};
--- src/pages/api/merchants/join-waitlist.ts
+++ src/pages/api/merchants/join-waitlist.ts
@@ -3,14 +3,14 @@ import type { APIRoute } from 'astro';
 
 export const prerender = false;
 
-const resend = new Resend(import.meta.env.RESEND_CUSTOMER_API_KEY);
+const resend = new Resend(import.meta.env.RESEND_MERCHANT_API_KEY);
 
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
 
   const response = await resend.contacts.create({
     email: body.email,
-    audienceId: import.meta.env.RESEND_LOOPLY_AUDIENCE,
+    audienceId: import.meta.env.RESEND_LOOPLY_MERCHANT_AUDIENCE,
   });
 
   if (response.error) {
